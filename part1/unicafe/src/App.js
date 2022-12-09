a2c03981b533cc77174d31660a3551497b5b47f8
softcat477 <softcat477@gmail.com>
2022-12-09 13:21:28 -0500

    Add part1-1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,18 +16,7 @@ const Feedback = ({text, score}) => {
   )
 }
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const clickFactory = (state, setState) => {
-    return () => {
-      setState(state+1)
-    }
-  }
-
+const Statistics = ({good, bad, neutral}) => {
   const avg = () => {
     if (good+bad+neutral == 0){
       return 0.0
@@ -43,18 +32,37 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>give feedback</h1>
-      <Button onClick={clickFactory(good, setGood)} text="good" />
-      <Button onClick={clickFactory(neutral, setNeutral)} text="neutral" />
-      <Button onClick={clickFactory(bad, setBad)} text="bad" />
-      <h2>statistics</h2>
+    <>
       <Feedback text="good" score={good} />
       <Feedback text="neutral" score={neutral} />
       <Feedback text="bad" score={bad} />
       <Feedback text="all" score={good+bad+neutral} />
       <Feedback text="average" score={avg()} />
       <Feedback text="positive" score={positive()} />
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const clickFactory = (state, setState) => {
+    return () => {
+      setState(state+1)
+    }
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={clickFactory(good, setGood)} text="good" />
+      <Button onClick={clickFactory(neutral, setNeutral)} text="neutral" />
+      <Button onClick={clickFactory(bad, setBad)} text="bad" />
+      <h2>statistics</h2>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
