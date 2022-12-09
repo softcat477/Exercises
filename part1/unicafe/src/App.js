435aee50a52012e65ec48b0ea3f87efbbb44ce03
softcat477 <softcat477@gmail.com>
2022-12-09 13:06:35 -0500

    Add part1-1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -28,6 +28,20 @@ const App = () => {
     }
   }
 
+  const avg = () => {
+    if (good+bad+neutral == 0){
+      return 0.0
+    }
+    return (good-bad)/(good+bad+neutral)
+  }
+
+  const positive = () => {
+    if (good+bad+neutral == 0){
+      return "0 %"
+    }
+    return ((good) / (good+bad+neutral) * 100).toString() + " %"
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -38,6 +52,9 @@ const App = () => {
       <Feedback text="good" score={good} />
       <Feedback text="neutral" score={neutral} />
       <Feedback text="bad" score={bad} />
+      <Feedback text="all" score={good+bad+neutral} />
+      <Feedback text="average" score={avg()} />
+      <Feedback text="positive" score={positive()} />
     </div>
   )
 }
