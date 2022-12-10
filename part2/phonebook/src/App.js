0f53a137dcaf093edcc43db87c5bb63d086f4315
softcat477 <softcat477@gmail.com>
2022-12-10 15:05:06 -0500

    Add part 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,22 +4,36 @@ const ListPerson = ({persons}) => {
   return (
     <>
       {persons.map(x => 
-        <div key={x.name}>
-          {x.name}
+        <div key={x.id}>
+          {x.name} {x.number}
         </div>
       )}
     </>
   )
 }
 
+const Field = ({text, value, onChange}) => {
+  return (
+    <>
+      <div>
+        {text}: <input value={value} onChange={onChange}/>
+      </div>
+    </>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '1-234-5678', id:0 }
   ]) 
   const [newName, setNewName] = useState('') // Control the form input element
+  const [newNumber, setNewNumber] = useState("")
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
   }
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -30,7 +44,9 @@ const App = () => {
       alert (newName + " is already added to phonebook")
     }
     else{
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({name: newName, number:newNumber, id:persons.length}))
+      setNewNumber("")
+      setNewName("")
     }
   }
 
@@ -38,9 +54,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNewName}/>
-        </div>
+        <Field text="name" value={newName} onChange={handleNewName} />
+        <Field text="number" value={newNumber} onChange={handleNewNumber} />
         <div>
           <button type="submit">add</button>
         </div>
