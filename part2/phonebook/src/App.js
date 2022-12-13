a49c4ebd5fd3ded81d99162a814bec65221b38de
softcat477 <softcat477@gmail.com>
2022-12-13 10:19:56 -0500

    Add part 2.15/2.16/2.17/2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personServices from "./services/person"
 
-const ListPerson = ({persons}) => {
+const ListPerson = ({persons, deletePerson}) => {
+
+  // Show person in the phonebook
   return (
     <>
       {persons.map(x => 
         <div key={x.id}>
-          {x.name} {x.number}
+          {x.name} {x.number} &nbsp;
+          <button onClick={() => deletePerson(x.id)}>delete</button>
         </div>
       )}
     </>
@@ -14,6 +17,10 @@ const ListPerson = ({persons}) => {
 }
 
 const Field = ({text, value, onChange}) => {
+  // Used by PersonForm
+  //   <text>: [value]
+  //   e.g.
+  //     name: []
   return (
     <>
       <div>
@@ -44,39 +51,77 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("")
   const [query, setQuery] = useState("")
 
+  // Use query to filter person to show
   const personsToShown = query.length === 0? persons: persons.filter((x)=>x.name.toLowerCase().includes(query.toLowerCase()))
 
   // Get data from db.json
   useEffect(() => {
-    axios.get("http://localhost:3001/phonebook")
-         .then(response => {
-          console.log("promise fulfilled and get: ", response.data)
-          setPersons(response.data)
-         })
+    personServices.getAll()
+                  .then(phonebook => {
+                    console.log("Get ", phonebook, " from DB.")
+                    setPersons(phonebook)
+                  })
   }, [])
 
-  // Callback func
+  // Input new name
   const handleNewName = (event) => {
     setNewName(event.target.value)
   }
+  // Input new number
   const handleNewNumber = (event) => {
     setNewNumber(event.target.value)
   }
+  // Input query
   const handleQuery = (event) => {
     setQuery(event.target.value)
   }
+  // Add (Submit) a person to phonebook
   const handleSubmit = (event) => {
     event.preventDefault()
 
     let exist_name = persons.map(x=>x.name)
 
     if (exist_name.includes(newName)){
-      alert (newName + " is already added to phonebook")
+      // Ask if the user wants to update this person's info
+      if (window.confirm(newName + " is already added to phonebook, replace the old number with a new one?")){
+        const person = persons.find(p => p.name === newName)
+        const changed_person = {...person, number:newNumber}
+
+        personServices.updatePerson(changed_person.id, changed_person)
+                       .then(returned_person => {
+                        setPersons(persons.map(p => p.name === newName? returned_person: p))
+                        setNewNumber("")
+                        setNewName("")
+                       })
+      }
     }
     else{
-      setPersons(persons.concat({name: newName, number:newNumber, id:persons.length}))
-      setNewNumber("")
-      setNewName("")
+      const new_id = Math.max(...persons.map(x => x.id)) + 1
+      const new_person = {name: newName, number:newNumber, id:new_id}
+      // Save this new person to DB
+      personServices.createNew(new_person)
+                    .then(ret_new_person => {
+                      setPersons(persons.concat(ret_new_person))
+                      setNewNumber("")
+                      setNewName("")
+                    })
+    }
+  }
+  // Delete a person
+  const deletePerson = (id) => {
+    const perrson_to_delete = persons.filter(x => x.id === id)[0]
+    if (window.confirm(`Do you want to delete ${perrson_to_delete.name}?`)){
+      personServices.deletePerson(id)
+                    .then(() => {
+                      const new_persons = persons.filter(x => x.id !== id)
+                      setPersons(new_persons)
+                    })
+                    .catch( error => {
+                      const ghost = persons.filter(x => x.id === id)
+                      alert (`Cannot delete ${ghost[0].name}`)
+                      const new_persons = persons.filter(x => x.id !== id)
+                      setPersons(new_persons)
+                    })
     }
   }
 
@@ -92,7 +137,7 @@ const App = () => {
                   newNumber={newNumber} handleNewNumber={handleNewNumber} />
 
       <h2>Numbers</h2>
-      <ListPerson persons={personsToShown} />
+      <ListPerson persons={personsToShown} deletePerson={deletePerson} />
     </div>
   )
 }
---
+++ part2/phonebook/src/services/person.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/phonebook'
+
+// Get: Get all person
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then((response) => response.data.concat())
+}
+
+// Post: Create a new person
+const createNew = (new_person) => {
+    // Ask the db to create data for a new person
+    const request = axios.post(baseUrl, new_person)
+    return request.then((response) => response.data)
+}
+
+// Put: Update a person's info
+const updatePerson = (id, new_data) => {
+    const request = axios.put(`${baseUrl}/${id}`, new_data)
+    return request.then(response => response.data)
+}
+
+// Delete: Remove a person from this phonebook
+const deletePerson = (id) => {
+    return axios.delete(`${baseUrl}/${id}`)
+}
+
+export default {getAll, createNew, updatePerson, deletePerson}
