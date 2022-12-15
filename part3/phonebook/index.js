e51daf21bde22e4c108710eae39847d258ef0238
softcat477 <softcat477@gmail.com>
2022-12-15 16:59:19 -0500

    Add 3.15, 3.16
    - Delete a person from MongoDB
    - Add middleware to handle error in
    - (1) Delete with ID
    - (2) Fetch with ID

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,31 +36,6 @@ app.use(cors())
 // Middleware: Serve static files from the backend
 app.use(express.static("build"))
 
-/*
-let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-*/
-
 // fetch all and return saved phonebook from MongoDB
 app.get("/api/persons", (request, response) => {
     // Return saved phonebook
@@ -75,18 +50,21 @@ app.get("/info", (request, response) => {
     response.send(`<p>${msg}</p><p>${time}</p>`)
 } )
 // fetch a person from MongoDB
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
     const req_id = request.params.id
-    Person.findById(req_id).then(person => {
-        response.json(person)
-    })
+    Person.findById(req_id)
+          .then(person => {
+                response.json(person)
+            })
+          .catch(error => next(error))
 })
-// Delete a person
-app.delete("/api/persons/:id", (request, response) => {
-    const req_id = Number(request.params.id)
-    phonebook = phonebook.filter(x => x.id !== req_id)
-
-    response.status(204).end()
+// Delete a person from MongoDB
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+           .then(result => {
+            response.status(204).end()
+           })
+           .catch(error => next(error))
 })
 // Add a person to MongoDB
 app.post("/api/persons", (request, response) => {
@@ -107,13 +85,23 @@ app.post("/api/persons", (request, response) => {
     }
 })
 
-// Our middleware to handle unknown endpoint
+// Middleware to handle unknown endpoint
 // Need to put this after all routes
 const unknownEndpoint = (request, response) => {
     response.status(404).send({error: "unknown endpoint"})
 }
-
 app.use(unknownEndpoint)
+
+// Middleware: Error Handling
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name == "CastError"){
+        return response.status(400).send({error: "malformatted id"})
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
