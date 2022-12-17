fa85120c9f92e7cb6437866cae06a1f236de8676
softcat477 <softcat477@gmail.com>
2022-12-17 17:02:43 -0500

    Add 4.7 return author with the mose likes

--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -25,9 +25,29 @@ const mostBlogs = (blog_list) => {
     }
 }
 
+const mostLiked = (blog_list) => {
+    const tmp = _.reduce(blog_list, function(result, entry){
+        const new_key = entry.author
+        const likes = entry.likes
+        if (new_key in result === false){
+            result[new_key] = 0
+        }
+        result[new_key] = result[new_key] + likes
+        return result
+    }, {})
+
+    const max_count = Math.max(...Object.values(tmp))
+    const author_name = Object.keys(tmp).find(key => tmp[key] === max_count)
+    return {
+        author: author_name,
+        likes: max_count
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLiked
 }
