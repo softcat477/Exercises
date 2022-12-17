f7515bfe10d7c87ebd91416acf69eba785cf7105
softcat477 <softcat477@gmail.com>
2022-12-17 12:47:51 -0500

    Add 4.4

--- part4/BlogList/tests/list.test.js
+++ part4/BlogList/tests/list.test.js
@@ -4,3 +4,31 @@ test("dummy returns one", () => {
     const blogs = []
     expect(listHelper.dummy(blogs)).toBe(1)
 })
+
+describe ("Total likes", () => {
+    const list_with_one_blog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test("when list has zero blog", () => {
+        const result = listHelper.totalLikes([])
+        expect(result).toBe(0)
+    })
+
+    test("when list has only one blog, equals the likes of that", () => {
+        const result = listHelper.totalLikes(list_with_one_blog)
+        expect(result).toBe(5)
+    })
+
+    test("of a list with multiple blogs", () => {
+        const result = listHelper.totalLikes(list_with_one_blog.concat(list_with_one_blog))
+        expect(result).toBe(10)
+    })
+})
--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs_list) => {
+    const sum = blogs_list.map(x => x.likes).reduce((total, current) => total + current, 0)
+    return sum
+} 
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
