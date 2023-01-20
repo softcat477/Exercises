ed3163c4f460cc6edde1ba82e55701372a4e23e1
softcat477 <softcat477@gmail.com>
2023-01-20 15:31:19 -0500

    Add 5.17 Backend support POST /api/testing/reset to clear DB
    - let cypress to bypass UI

--- part4/BlogList/app.js
+++ part4/BlogList/app.js
@@ -6,6 +6,7 @@ const blogRouter = require("./controllers/blog")
 const userRouter = require("./controllers/users")
 const loginRouter = require("./controllers/login")
 const middleware = require("./utils/middleware")
+const config = require("./utils/config")
 
 app.use(cors())
 app.use(express.json())
@@ -13,6 +14,11 @@ app.use(middleware.getToken)
 app.use("/api/blogs", middleware.getUserIdFromToken, blogRouter)
 app.use("/api/users", userRouter)
 app.use("/api/login", loginRouter)
+if(config.MODE == "test"){
+    // POST http://localhost:8080/api/testing/reset to clear database
+    const testingRouter = require("./controllers/testing")
+    app.use("/api/testing", testingRouter)
+}
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- part4/BlogList/utils/config.js
+++ part4/BlogList/utils/config.js
@@ -6,5 +6,6 @@ MONGODB_URL = process.env.NODE_ENV === "test" ?
     process.env.MONGODB_URL
 
 SECRET = process.env.SECRET
+MODE = process.env.NODE_ENV
 
-module.exports = {PORT, MONGODB_URL, SECRET}
+module.exports = {PORT, MONGODB_URL, SECRET, MODE}
