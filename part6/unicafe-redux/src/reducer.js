6fcfc95a75ebfb7058c8e9de8d2793cc1789ffa9
softcat477 <softcat477@gmail.com>
2023-01-24 14:18:21 -0500

    Add 6.1: finish reducer and its test
    - npm install deep-freeze --save-dev

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD': {
+      const new_state = {...state, good:state.good + 1}
+      return new_state
+    }
+    case 'OK': {
+      const new_state = {...state, ok:state.ok + 1}
+      return new_state
+    }
+    case 'BAD': {
+      const new_state = {...state, bad:state.bad + 1}
+      return new_state
+    }
+    case 'ZERO': {
+      const new_state = initialState
+      return new_state
+    }
+    default: return state
+  }
+  
+}
+
+export default counterReducer
