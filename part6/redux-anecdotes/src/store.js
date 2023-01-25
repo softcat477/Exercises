91e7bb6e6d4e031542fab71bf623df5664a96298
softcat477 <softcat477@gmail.com>
2023-01-25 12:38:49 -0500

    Add 6.10: Add Notification to App
    - store.js: Add notification's reducer to configureStore'

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import reducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 const store = configureStore({
   reducer: {
-    notes: reducer
+    notes: reducer,
+    notification: notificationReducer
   }
 })
 
