22b213f63c9c13a3180f58ccfa754fd4388be24f
softcat477 <softcat477@gmail.com>
2023-01-25 13:29:18 -0500

    Add 6.12: Add Filter componentwq

---
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -0,0 +1,17 @@
+const Filter = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div stype={style}>
+      filter: <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Filter
