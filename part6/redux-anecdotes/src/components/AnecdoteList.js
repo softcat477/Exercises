b9713facb667ec3d019c2b39be1d232e2015f4f6
softcat477 <softcat477@gmail.com>
2023-01-26 09:57:01 -0500

    change front-end layout

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -6,10 +6,7 @@ const Anecdote = ({anecdote, handleClick}) => {
   return (
     <div key={anecdote.id}>
       <div>
-        {anecdote.content}
-      </div>
-      <div>
-        has {anecdote.votes}
+        {anecdote.content} - has {anecdote.votes}
         <button onClick={handleClick}>vote</button>
       </div>
     </div>
